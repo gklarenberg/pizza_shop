d40314e8d62f41274e051cf982b2eb0fbf36b982
John Buckley <john.buckley@ufl.edu>
2023-10-29 13:56:11 -0400

    Created a new data set using full_join() to join all 16 datasets for pizza sales. Need to find a more effecient way to perform the join.

--- sales/sales_script.R
+++ sales/sales_script.R
@@ -18,9 +18,74 @@ for (salesdata in all_sales){
 ############## JOIN ###############
 # Use a tidyverse join to join all the data together into one file
 # called sales_data, then run the rest of the code
+sales_data_full <- full_join(datafile,dataset_1, by = c("day","month","year",
+                                                        "pizza","number")) 
+sales_data_full <- full_join(sales_data_full,dataset_2, 
+                             by = c("day","month","year",
+                                    "pizza","number"))
+sales_data_full <- full_join(sales_data_full,dataset_3, 
+                             by = c("day","month","year",
+                                    "pizza","number"))
+sales_data_full <- full_join(sales_data_full,dataset_4, 
+                             by = c("day","month","year",
+                                    "pizza","number"))
+sales_data_full <- full_join(sales_data_full,dataset_5, 
+                             by = c("day","month","year",
+                                    "pizza","number"))
+sales_data_full <- full_join(sales_data_full,dataset_6, 
+                             by = c("day","month","year",
+                                    "pizza","number"))
+sales_data_full <- full_join(sales_data_full,dataset_7, 
+                             by = c("day","month","year",
+                                    "pizza","number"))
+sales_data_full <- full_join(sales_data_full,dataset_8, 
+                             by = c("day","month","year",
+                                    "pizza","number"))
+sales_data_full <- full_join(sales_data_full,dataset_9, 
+                             by = c("day","month","year",
+                                    "pizza","number"))
+sales_data_full <- full_join(sales_data_full,dataset_10, 
+                             by = c("day","month","year",
+                                    "pizza","number"))
+sales_data_full <- full_join(sales_data_full,dataset_11, 
+                             by = c("day","month","year",
+                                    "pizza","number"))
+sales_data_full <- full_join(sales_data_full,dataset_12, 
+                             by = c("day","month","year",
+                                    "pizza","number"))
+sales_data_full <- full_join(sales_data_full,dataset_13, 
+                             by = c("day","month","year",
+                                    "pizza","number"))
+sales_data_full <- full_join(sales_data_full,dataset_14, 
+                             by = c("day","month","year",
+                                    "pizza","number"))
+sales_data_full <- full_join(sales_data_full,dataset_15, 
+                             by = c("day","month","year",
+                                    "pizza","number"))
+sales_data_full <- full_join(sales_data_full,dataset_16, 
+                             by = c("day","month","year",
+                                    "pizza","number"))
+head(sales_data_full)
+tail(sales_data_full)
+sales_data <- sales_data_full
 
+## The warning message that R gives us prompts the user to verify the 
+## the relationship between x and y. Specifically this warning is becuase the 
+## pizza number variable for each of the data tables being joined are often 
+## very similar. For example, the first vegetarian pizza sold on two seperate 
+## days will both recieve the pizza number of 1. R sees this as a "many-to-
+## many" relationship, which triggers the warning message. 
 
+## The final dataset has 6 columns because "dataset_15" contained 6 columns,
+## unlike the other datasets which only contained 5. The full_join function 
+## implies that we want to keep ALL information, so it created an extra column
+## for all datasets used in the full_join. Where they didnt have a value for that
+## column, R assigns "NA".
 
+## This data looks misleading at first glance because of the way the data is 
+## keeping track of the pizza number variable. It seems that the pizza number
+## is stating what number of that type of pizza sold on that day, not that the
+## pizzeria sold more than one pizza per row. 
 ########################################
 
 ##### 3. Create summaries #####
@@ -54,3 +119,4 @@ sales_ave_daily <- sales_data %>%
 
 ggplot(data = sales_ave_daily, aes(x = date, y = ave_sales, fill = pizza))+
   geom_bar(stat = "identity", position = "dodge")
+
